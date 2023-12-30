aa31eeb22ce7ec2ae26e2dfbdf18749423fa9e40
tiennguyen00 <18521487@gm.uit.edu.vn>
2023-12-31 00:10:00 +0700

    update gen point cloud model

--- app/(not-tunnel-rat)/morph-particles/page.tsx
+++ app/(not-tunnel-rat)/morph-particles/page.tsx
@@ -1,6 +1,15 @@
 'use client'
 
-import { Points, Scroll, ScrollControls, useFBO, useGLTF, useScroll } from '@react-three/drei'
+import {
+  Points,
+  Scroll,
+  ScrollControls,
+  useFBO,
+  useGLTF,
+  useScroll,
+  OrbitControls,
+  PointMaterial,
+} from '@react-three/drei'
 import { Common } from '@/components/canvas/View'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import * as THREE from 'three'
@@ -9,6 +18,7 @@ import vertexShader from '@/templates/Shader/glsl/particles-simulation.vert'
 import fragmentShader from '@/templates/Shader/glsl/particles-simulation.frag'
 import vertextParRender from '@/templates/Shader/glsl/particles-render.vert'
 import fragmentParRender from '@/templates/Shader/glsl/particles-render.frag'
+import { getModelGeometry } from '@/utils/shared'
 
 const makeTexture = (g) => {
   let vertAmount = g.attributes.position.count
@@ -60,17 +70,22 @@ const makeTexture = (g) => {
 }
 
 const MainBody = () => {
-  const dog = useGLTF('/models/dog.glb')
-  const dogGeometry = dog.scene.children[0].geometry
-  const refGeoParticles = useRef<THREE.BufferGeometry>(null)
-  const refPoints = useRef(null)
-  const {
-    size: { width, height },
-  } = useThree()
+  const width = 256,
+    height = 256
+  const three = useThree()
 
-  const uTextureA = makeTexture(dogGeometry)
-  const data = useScroll()
-  const renderTarger = useFBO({
+  const conan = useGLTF('/models/boy.glb')
+  const conanGeometry = useMemo(() => {
+    const merge = getModelGeometry(conan.nodes)
+    return merge
+  }, [conan.nodes])
+
+  const refGeoParticles = useRef<THREE.BufferGeometry>(null)
+  const refGeoParticlesP = useRef<THREE.BufferGeometry>(null)
+
+  const uTextureA = makeTexture(conanGeometry)
+  // const data = useScroll()
+  let renderTarget = useFBO(width, height, {
     minFilter: THREE.NearestFilter, // Important because we want to sample square pixels
     magFilter: THREE.NearestFilter,
     generateMipmaps: false, // No need
@@ -81,7 +96,7 @@ const MainBody = () => {
     type: THREE.FloatType, // Important because we need precise coordinates (not ints)
   })
 
-  const shaderArgs = useMemo(
+  const simGeoParticles = useMemo(
     () => ({
       uniforms: {
         uTextureA: { value: uTextureA },
@@ -95,82 +110,100 @@ const MainBody = () => {
       vertexShader,
       fragmentShader,
     }),
+    [uTextureA],
+  )
+  useEffect(() => {
+    if (refGeoParticles.current) {
+      const positions = new Float32Array([-1, -1, 0, 1, -1, 0, 1, 1, 0, -1, -1, 0, 1, 1, 0, -1, 1, 0])
+      const uv = new Float32Array([0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0])
+      refGeoParticles.current.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+      refGeoParticles.current.setAttribute('uv', new THREE.BufferAttribute(uv, 2))
+    }
+
+    if (refGeoParticlesP.current) {
+      const length = width * height
+      let vertices = new Float32Array(length * 3)
+      for (let i = 0; i < length; i++) {
+        let i3 = i * 3
+        vertices[i3 + 0] = (i % width) / width
+        vertices[i3 + 1] = i / width / height
+        vertices[i3 + 2] = 0
+      }
+      refGeoParticlesP.current.setAttribute('position', new THREE.BufferAttribute(vertices, 3))
+    }
+  }, [])
+
+  const renderMatParticles = useMemo(
+    () => ({
+      uniforms: {
+        uPositions: { value: null },
+        uSize: { value: 12 },
+        uTime: { value: 0 },
+        uPixelRatio: { value: Math.min(window.devicePixelRatio, 2) },
+        uScroll: { value: 0.0 },
+      },
+      defines: {
+        uTotalModels: parseFloat('1').toFixed(2),
+        uRange: 1.0,
+      },
+      vertexShader: vertextParRender,
+      fragmentShader: fragmentParRender,
+      transparent: true,
+      depthWrite: false,
+      blending: THREE.AdditiveBlending,
+    }),
     [],
   )
 
-  const renderMaterial = new THREE.ShaderMaterial({
-    uniforms: {
-      uPositions: { value: null },
-      uSize: { value: 12 },
-      uTime: { value: 0 },
-      uPixelRatio: { value: Math.min(window.devicePixelRatio, 2) },
-      uScroll: { value: 1.0 },
-    },
-    defines: {
-      uTotalModels: parseFloat('1').toFixed(2),
-      uRange: 1.0,
-    },
-    vertexShader: vertextParRender,
-    fragmentShader: fragmentParRender,
-    transparent: true,
-    depthWrite: false,
-    blending: THREE.AdditiveBlending,
-  })
+  useFrame(({ gl, camera, scene }) => {
+    gl.setRenderTarget(renderTarget)
+    gl.clear()
+    gl.render(scene, camera)
+    gl.setRenderTarget(null)
 
-  const particlesGeometry = useMemo(() => {
-    const length = width * height
-    let vertices = new Float32Array(length * 3)
-    for (let i = 0; i < length; i++) {
-      let i3 = i * 3
-      vertices[i3 + 0] = (i % width) / width
-      vertices[i3 + 1] = i / width / height
+    if (refPoint.current) {
+      refPoint.current.material.uniforms.uPositions.value = renderTarget.texture
     }
-
-    const geometry = new THREE.BufferGeometry()
-    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3))
-    return geometry
-  }, [width, height])
-
-  useEffect(() => {
-    if (!refGeoParticles.current) return
-
-    const positions = new Float32Array([-1, -1, 0, 1, -1, 0, 1, 1, 0, -1, -1, 0, 1, 1, 0, -1, 1, 0])
-    const uv = new Float32Array([0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0])
-    refGeoParticles.current.setAttribute('position', new THREE.BufferAttribute(positions, 3))
-    refGeoParticles.current.setAttribute('uv', new THREE.BufferAttribute(uv, 2))
-  }, [])
-
-  useFrame((state) => {
-    state.gl.setRenderTarget(renderTarger)
-    state.gl.clear()
-    state.gl.render(state.scene, state.camera)
-    state.gl.setRenderTarget(null)
   })
+
+  const refPoint = useRef<THREE.Points<THREE.BufferGeometry<THREE.NormalBufferAttributes>, THREE.ShaderMaterial>>(null)
 
   return (
     <>
       <mesh>
         <bufferGeometry ref={refGeoParticles} />
-        <shaderMaterial args={[shaderArgs]} />
+        <shaderMaterial args={[simGeoParticles]} />
       </mesh>
-
-      <Points geometry={particlesGeometry} material={renderMaterial} />
-      {/* <mesh>
-        <shaderMaterial />
-      </mesh> */}
+      <points ref={refPoint}>
+        <bufferGeometry ref={refGeoParticlesP} />
+        <shaderMaterial args={[renderMatParticles]} />
+      </points>
     </>
   )
 }
 
+const Helu = () => {
+  return <MainBody />
+}
+
 const Page = () => {
   return (
-    <Canvas id='canvas-custom' className='flex h-full w-full flex-col items-center justify-center'>
+    <Canvas
+      id='canvas-custom'
+      className='flex h-full w-full flex-col items-center justify-center'
+      gl={{
+        powerPreference: 'high-performance',
+        antialias: false,
+        alpha: false,
+      }}
+    >
       <ScrollControls pages={4} damping={0.1}>
         <Common />
         <Scroll>
-          <MainBody />
+          <Helu />
         </Scroll>
       </ScrollControls>
+      <OrbitControls />
     </Canvas>
   )
 }
