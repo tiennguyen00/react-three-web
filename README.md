caf1029fa7dd97c4ad3bd2d08788339773a89ca3
tiennguyen00 <18521487@gm.uit.edu.vn>
2024-01-30 11:19:06 +0700

    chore

--- README.md
+++
@@ -1,3 +0,0 @@
-# react-three-web
-
-This project was bootstrapped with [`create-r3f-app`](https://github.com/utsuboco/create-r3f-app)
--- src/helpers/components/Three.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { r3f } from '@/helpers/global'
-import { PropsWithChildren } from 'react'
-
-export const Three = ({ children }: PropsWithChildren) => {
-  return <r3f.In>{children}</r3f.In>
-}
