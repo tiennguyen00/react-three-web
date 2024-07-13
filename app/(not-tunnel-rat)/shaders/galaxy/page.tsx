d13c19a28fe7ba7900b08ab13d6eee82b2f4fddb
Hubert Nguyen <18521487@gm.uit.edu.vn>
2024-07-13 16:50:44 +0700

    update: create spin effect for galaxy

--- app/(not-tunnel-rat)/shaders/galaxy/page.tsx
+++ app/(not-tunnel-rat)/shaders/galaxy/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 import { OrbitControls } from '@react-three/drei'
-import { Canvas, useThree } from '@react-three/fiber'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { useControls } from 'leva'
-import React, { useEffect, useMemo } from 'react'
+import React, { useEffect, useMemo, useRef } from 'react'
 import * as THREE from 'three'
 import vertex from '@/components/shared/galaxy/galaxy.vert'
 import fragment from '@/components/shared/galaxy/galaxy.frag'
@@ -11,10 +11,12 @@ const PointSpin = () => {
   const {
     gl: { getPixelRatio },
   } = useThree()
+  const pointRef = useRef<THREE.Points | null>(null)
+
   const controls = useControls('Points', {
     count: 200000,
     size: {
-      value: 5,
+      value: 50,
       step: 0.5,
     },
     radius: 5,
@@ -28,10 +30,11 @@ const PointSpin = () => {
 
   const { count, radius, branches, randomnessPower, randomness, insideColor, outsideColor, size } = controls
 
-  const { positions, colors, scales } = useMemo(() => {
+  const { positions, colors, scales, aRandomness } = useMemo(() => {
     const positions = new Float32Array(count * 3)
     const colors = new Float32Array(count * 3)
     const scales = new Float32Array(count)
+    const aRandomness = new Float32Array(count * 3)
 
     const insideColorV = new THREE.Color(insideColor)
     const outsideColorV = new THREE.Color(outsideColor)
@@ -43,13 +46,18 @@ const PointSpin = () => {
       const radiusR = Math.random() * radius
       const branchAngle = ((i % branches) / branches) * Math.PI * 2
 
-      const randomX = Math.pow(Math.random(), randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * randomness * radiusR
-      const randomY = Math.pow(Math.random(), randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * randomness * radiusR
-      const randomZ = Math.pow(Math.random(), randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * randomness * radiusR
+      positions[i3] = Math.cos(branchAngle) * radiusR
+      positions[i3 + 1] = 0.0
+      positions[i3 + 2] = Math.sin(branchAngle) * radiusR
 
-      positions[i3] = Math.cos(branchAngle) * radiusR + randomX
-      positions[i3 + 1] = randomY
-      positions[i3 + 2] = Math.sin(branchAngle) * radiusR + randomZ
+      // Randomness
+      const randomX = Math.pow(Math.random(), randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * radiusR
+      const randomY = Math.pow(Math.random(), randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * radiusR
+      const randomZ = Math.pow(Math.random(), randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * radiusR
+
+      aRandomness[i3] = randomX
+      aRandomness[i3 + 1] = randomY
+      aRandomness[i3 + 2] = randomZ
 
       // Color
       const mixedColor = insideColorV.clone()
@@ -67,12 +75,14 @@ const PointSpin = () => {
       positions,
       colors,
       scales,
+      aRandomness,
     }
   }, [branches, count, insideColor, outsideColor, radius, randomness, randomnessPower])
 
   const uniforms = useMemo(
     () => ({
       uSize: { value: 5 * getPixelRatio() },
+      uTime: { value: 0 },
     }),
     [],
   )
@@ -81,12 +91,19 @@ const PointSpin = () => {
     uniforms.uSize.value = size
   }, [size])
 
+  useFrame((_, delta) => {
+    if (!pointRef.current) return
+    const t = _.clock.getElapsedTime()
+    ;(pointRef.current?.material as THREE.ShaderMaterial).uniforms.uTime.value = t
+  })
+
   return (
-    <points>
+    <points ref={pointRef}>
       <bufferGeometry key={`${Object.entries(controls).flat(Infinity)}`}>
         <bufferAttribute attach='attributes-position' count={count} array={positions} itemSize={3} />
         <bufferAttribute attach='attributes-color' count={count} array={colors} itemSize={3} />
         <bufferAttribute attach='attributes-aScale' count={count} array={scales} itemSize={1} />
+        <bufferAttribute attach='attributes-aRandomness' count={count} array={aRandomness} itemSize={3} />
       </bufferGeometry>
       {/* <pointsMaterial size={size} sizeAttenuation depthWrite={false} blending={THREE.AdditiveBlending} vertexColors /> */}
       <shaderMaterial
--- src/components/shared/galaxy/galaxy.frag
+++ src/components/shared/galaxy/galaxy.frag
@@ -1,5 +1,13 @@
+varying vec3 vColor;
+
 void main()
 {
-  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+  // Disc
+  float strength = distance(gl_PointCoord, vec2(0.5));
+  strength = 1.0 - strength;
+  strength = pow(strength, 10.0);
+
+  vec3 color = mix(vec3(0.0), vColor, strength);
+  gl_FragColor = vec4(color, 1.0);
   #include <colorspace_fragment>
 }
