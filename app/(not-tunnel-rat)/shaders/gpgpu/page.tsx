dad06d8ee78a810efbaaacc7b7a5b6413999fafd
Hubert Nguyen <18521487@gm.uit.edu.vn>
2024-09-05 15:23:00 +0700

    update: implement GPGPU tech

--- app/(not-tunnel-rat)/shaders/gpgpu/page.tsx
+++ app/(not-tunnel-rat)/shaders/gpgpu/page.tsx
@@ -1,12 +1,12 @@
 'use client'
-import { OrbitControls, shaderMaterial } from '@react-three/drei'
+import { OrbitControls, shaderMaterial, useGLTF } from '@react-three/drei'
 import { Canvas, extend, Object3DNode, useFrame, useThree } from '@react-three/fiber'
 import vertex from '@/components/shared/gpgpu/gpgpu.vert'
 import fragment from '@/components/shared/gpgpu/gpgpu.frag'
 import gpgpuParticlesShader from '@/components/shared/gpgpu/particles.glsl'
 
 import * as THREE from 'three'
-import { useEffect, useMemo, useRef } from 'react'
+import { useMemo, useRef } from 'react'
 import { GPUComputationRenderer } from 'three/examples/jsm/misc/GPUComputationRenderer.js'
 
 const GpgpuMaterial = shaderMaterial(
@@ -16,6 +16,7 @@ const GpgpuMaterial = shaderMaterial(
       window.innerWidth * window.devicePixelRatio,
       window.innerHeight * window.devicePixelRatio,
     ),
+    uParticlesTexture: new THREE.Texture(),
   },
   vertex,
   fragment,
@@ -33,35 +34,64 @@ const Experience = () => {
   const { gl } = useThree()
 
   const { gpuCompute, particlesVariable } = useMemo(() => {
-    const count = pointRef.current?.geometry.attributes.position.count ?? 0
+    /**
+     * Base geometry
+     */
+    const baseGeometry: any = {}
+    baseGeometry.instance = new THREE.SphereGeometry(3)
+    baseGeometry.count = baseGeometry.instance.attributes.position.count
 
-    const size = Math.ceil(Math.sqrt(count))
+    const size = Math.ceil(Math.sqrt(baseGeometry.count))
 
     const gpuCompute = new GPUComputationRenderer(size, size, gl)
-    // It’s a DataTexture which is similar to other Three.js textures but the pixels data is set up as an array which we can access in
+    // It’s a DataTexture which is similar to other Three.js textures but the pixels data is set up as an array which we can access in "baseParticleTexture.image.data"
     const baseParticleTexture = gpuCompute.createTexture()
-    for (let i = 0; i < count; i++) {
+    for (let i = 0; i < baseGeometry.count; i++) {
       const i3 = i * 3
       const i4 = i * 4
 
       //
-      baseParticleTexture.image.data[i4 + 0] = pointRef.current?.geometry.attributes.position.array[i3 + 0] as number
-      baseParticleTexture.image.data[i4 + 1] = pointRef.current?.geometry.attributes.position.array[i3 + 1] as number
-      baseParticleTexture.image.data[i4 + 2] = pointRef.current?.geometry.attributes.position.array[i3 + 2] as number
+      baseParticleTexture.image.data[i4 + 0] = baseGeometry.instance.attributes.position.array[i3 + 0] as number
+      baseParticleTexture.image.data[i4 + 1] = baseGeometry.instance.attributes.position.array[i3 + 1] as number
+      baseParticleTexture.image.data[i4 + 2] = baseGeometry.instance.attributes.position.array[i3 + 2] as number
       baseParticleTexture.image.data[i4 + 3] = 0
     }
 
     // Particles variables, GPU interact with those variables
+    // "uParticles" (texture) is the name of the variable, it will be injected in the shader
     const particlesVariable = gpuCompute.addVariable('uParticles', gpgpuParticlesShader, baseParticleTexture)
+
     // we want the particles to persist in time, meaning that their coordinates will be saved and re-used in the next frame, The “variable” needs to be re-injected into itself
     gpuCompute.setVariableDependencies(particlesVariable, [particlesVariable])
     gpuCompute.init()
+
+    const particlesUvArray = new Float32Array(baseGeometry.count * 2)
+    for (let y = 0; y < size; y++) {
+      for (let x = 0; x < size; x++) {
+        const i = y * size + x
+        const i2 = i * 2
+
+        const uvX = (x + 0.5) / size
+        const uvY = (y + 0.5) / size
+
+        particlesUvArray[i2 + 0] = uvX
+        particlesUvArray[i2 + 1] = uvY
+      }
+    }
+    // console.table(particlesUvArray)
+
+    pointRef.current?.geometry.setDrawRange(0, baseGeometry.count)
+    pointRef.current?.geometry.setAttribute('aParticlesUv', new THREE.BufferAttribute(particlesUvArray, 2))
 
     return { gpuCompute, particlesVariable }
   }, [pointRef.current])
 
   useFrame((_, delta) => {
     gpuCompute?.compute()
+    if (pointRef.current) {
+      const { uniforms } = pointRef.current.material as THREE.ShaderMaterial
+      uniforms.uParticlesTexture.value = gpuCompute?.getCurrentRenderTarget(particlesVariable).texture
+    }
   })
 
   return (
@@ -71,7 +101,7 @@ const Experience = () => {
         <meshBasicMaterial map={gpuCompute?.getCurrentRenderTarget(particlesVariable).texture} />
       </mesh>
       <points ref={pointRef}>
-        <sphereGeometry args={[3]} />
+        <bufferGeometry />
         <gpgpuMaterial />
       </points>
     </>
--- src/components/shared/gpgpu/particles.glsl
+++ src/components/shared/gpgpu/particles.glsl
@@ -2,9 +2,6 @@ void main()
 {
   vec2 uv = gl_FragCoord.xy / resolution.xy;
   vec4 particle = texture(uParticles, uv);
-  particle.y += 0.01;
+  // particle.y += 0.01;
   gl_FragColor = particle;
-
-  #include <tonemapping_fragment>
-  #include <colorspace_fragment>
 }
